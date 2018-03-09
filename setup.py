86e6611f869a4cac7a5a88c8a090a66c7caa94a5
sharath-thirunagaru <sharath@Sharaths-MacBook-Pro.local>
2018-03-09 11:12:37 -0600

    Initial setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='Hillsoft Labs',
+    version='0.0.1',
+    author='Sharath Thirunagaru',
+    author_email='sharath.tds@gmail.com',
+    description="Hillsoft Web Application for timesheets management",
+    packages=find_packages(exclude=[])
+)
